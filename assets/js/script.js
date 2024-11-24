3ae3e1cf5c4209e702c72307483972b8a04a1b99
agweb12 <151913740+agweb12@users.noreply.github.com>
2024-11-24 21:16:28 +0100

    version 1.0
    Gaming Space est un projet HTML CSS Javascript qui a pour but :
    
    1. De créer un menu en 3D, en utilisant la perspective et le translate Z X Y en CSS.
    2. De donner un aperçu des sections en 3D, en tant que nav
    3. De gérer l'ajout et la suppression des classes Html, nécessaires à la 3D, via le javascript
    4. De créer une expérience utilisateur agréable avec la propriété Css color-scheme disponible sur tous les navigateurs de février 2022. Cette optimisation permet un jeu de couleur selon le choix du mode que l'utilisateur a choisi sur son appareil : Clair ou Sombre.
    
    Ainsi, j'ai privilégié le color-scheme : light dark, pour jouer sur la couleur soit light, soit sombre.
    
    Cette propriété est utilisé dans le root, pour une meilleure manipulation des jeux de couleurs, via les variables.
    
    Exemple: 
    
    :root{
    color-scheme : light dark;
    --bg-primary: light-dark(#fff, #000);
    }
    
    la valeur light-dark sera composé de 2 couleurs. Aussi, vous pouvez l'utiliser pour différentes propriétés.

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleMenu = document.querySelector('.logo');
+    const perspective = document.querySelector('.perspective');
+    const outerNavReturn1 = document.querySelector('.outer-nav-return1');
+    const outerNavReturn2 = document.querySelector('.outer-nav-return2');
+    const outerNavReturn3 = document.querySelector('.outer-nav-return3');
+    const outerNavReturn4 = document.querySelector('.outer-nav-return4');
+    const outerNavReturn5 = document.querySelector('.outer-nav-return5');
+
+
+    function newNavigation() {
+        perspective.classList.add('modalview-perspective', 'effect-rotate-left-animate');
+        outerNavReturn1.classList.add('is-visual');
+        outerNavReturn2.classList.add('is-visual');
+        outerNavReturn3.classList.add('is-visual');
+        outerNavReturn4.classList.add('is-visual');
+        outerNavReturn5.classList.add('is-visual');
+
+    }
+
+    function navigationNormal() {
+        if (outerNavReturn1.classList.contains('is-visual')) {
+            perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
+            outerNavReturn1.classList.remove('is-visual');
+        }
+    }
+    function navigationNormal2() {
+        if (outerNavReturn2.classList.contains('is-visual')) {
+            perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
+            outerNavReturn2.classList.remove('is-visual');
+        }
+    }
+    function navigationNormal3() {
+        if (outerNavReturn3.classList.contains('is-visual')) {
+            perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
+            outerNavReturn3.classList.remove('is-visual');
+        }
+    }
+    function navigationNormal4() {
+        if (outerNavReturn4.classList.contains('is-visual')) {
+            perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
+            outerNavReturn4.classList.remove('is-visual');
+        }
+    }
+    function navigationNormal5() {
+        if (outerNavReturn5.classList.contains('is-visual')) {
+            perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
+            outerNavReturn5.classList.remove('is-visual');
+        }
+    }
+
+    toggleMenu.addEventListener('click', newNavigation);
+
+    outerNavReturn1.addEventListener('click', navigationNormal);
+    outerNavReturn2.addEventListener('click', navigationNormal2);
+    outerNavReturn3.addEventListener('click', navigationNormal3);
+    outerNavReturn4.addEventListener('click', navigationNormal4);
+    outerNavReturn5.addEventListener('click', navigationNormal5);
+
+});
