b99f47fbccb3c63ac6c32e9b4b3511ae2097cbe2
agweb12 <151913740+agweb12@users.noreply.github.com>
2024-11-25 17:00:06 +0100

    version 1.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const outerNavReturn3 = document.querySelector('.outer-nav-return3');
     const outerNavReturn4 = document.querySelector('.outer-nav-return4');
     const outerNavReturn5 = document.querySelector('.outer-nav-return5');
-
+    const targetServices = document.querySelector('#services');
 
     function newNavigation() {
         perspective.classList.add('modalview-perspective', 'effect-rotate-left-animate');
@@ -22,12 +22,17 @@ document.addEventListener('DOMContentLoaded', function () {
         if (outerNavReturn1.classList.contains('is-visual')) {
             perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
             outerNavReturn1.classList.remove('is-visual');
+            // appliquer un délai de 2 secondes
+            //puis injecter un #intro dans l'url
         }
     }
     function navigationNormal2() {
         if (outerNavReturn2.classList.contains('is-visual')) {
             perspective.classList.remove('modalview-perspective', 'effect-rotate-left-animate');
             outerNavReturn2.classList.remove('is-visual');
+
+            // appliquer un délai de 2 secondes
+            //puis injecter un #services dans l'url
         }
     }
     function navigationNormal3() {
